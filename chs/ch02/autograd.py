0a98c0238d98fb64a206bef51486a466da6a7491
jacky <jackyfan@163.com>
2025-03-05 14:29:36 +0800

    计算反向传播

---
+++ chs/ch02/autograd.py
@@ -0,0 +1,28 @@
+import torch
+
+x = torch.arange(4.0)
+print(x)
+
+x.requires_grad_(True)  # 等价于x=torch.arange(4.0,requires_grad=True)
+print(x.grad)  # 默认值是None
+y = 2 * torch.dot(x, x)
+print(y)
+
+y.backward()
+print(x.grad)
+
+# 在默认情况下，PyTorch会累积梯度，我们需要清除之前的值
+x.grad.zero_()
+y = x.sum()
+y.backward()
+print('x.sum grad:',x.grad)
+
+
+# 对非标量调用backward需要传入一个gradient参数，该参数指定微分函数关于self的梯度。
+# 本例只想求偏导数的和，所以传递一个1的梯度是合适的
+x.grad.zero_()
+print(x)
+y = x * x
+# 等价于y.backward(torch.ones(len(x)))
+y.sum().backward()
+print('x*x grad:',x.grad)
